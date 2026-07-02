8bb12c90b2195fcf175b1beb9f0911f6ab42c542
uzodimmavictor <uzodimmavictor7@gmail.com>
2026-07-02 14:32:59 +0200

    Refactor database interface classes to include database parameter and add new context.py file

---
+++ database/redis.py
@@ -0,0 +1,5 @@
+class RedisDB(DatabaseInterface):
+    def __init__(self, host, port, database, user, password):
+        super().__init__(host, port, user, password, database)
+        self.connection = None
+        
